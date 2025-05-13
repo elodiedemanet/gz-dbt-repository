ad5f2a2e361aa57c74c909fe59205f277a16fec9
Elodie Demanet <dodie.demanet@hotmail.com>
2025-05-13 10:20:41 +0000

    Summary Table

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -8,6 +8,13 @@ WITH calculs AS (
 SELECT 
     orders_id,
     date_date,
+    revenue, 
+    quantity, 
+    purchase_cost, 
+    margin,
+    shipping_fee,
+    ship_cost, 
+    log_cost,
     margin + shipping_fee - log_cost - ship_cost AS operational_margin  
 FROM calculs
 
