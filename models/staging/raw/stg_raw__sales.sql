0fe3c7dfe92fe1f74d53db6300b99dd4b9dc11f8
Elodie Demanet <dodie.demanet@hotmail.com>
2025-05-13 09:55:01 +0000

    Purchase cost and margin added

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -3,7 +3,6 @@ with
 source as (
 
     select * from {{ source('raw', 'sales') }}
-
 ),
 
 renamed as (
@@ -14,7 +13,6 @@ renamed as (
         pdt_id as products_id,
         revenue,
         quantity,
-        CONCAT(orders_id, '-',pdt_id) AS pk_order_product
     from source
 )
 
