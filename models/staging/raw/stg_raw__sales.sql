c7eb94b0bd98b59f2eec6ba77b3b2e2371a0273b
Elodie Demanet <dodie.demanet@hotmail.com>
2025-05-13 08:54:19 +0000

    Added column for PK

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -13,10 +13,9 @@ renamed as (
         orders_id,
         pdt_id as products_id,
         revenue,
-        quantity
-
+        quantity,
+        CONCAT(orders_id, '-',pdt_id) AS pk_order_product
     from source
-
 )
 
 select * from renamed
